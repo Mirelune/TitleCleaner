74a7f1f95091aa20872fecd8ec7279c912a42dec
Mirelune <yves.marnet@yahoo.fr>
2023-04-13 19:17:37 +0200

    Finished renaming logic

--- test_titleCleaner.py
+++ test_titleCleaner.py
@@ -1,16 +1,18 @@
-import os.path
+import os
 import glob
+import re
 
 extensionList = {'.mkv','.avi'}
 filmsPath = input('Please enter the path to the directory you want to order you movies in : ')
 isDir = os.path.exists(filmsPath)
-fileRegex = '[*]'
 filesList = []
 
 # Open folder
 if isDir :
     for extention in extensionList :
-        filesList+=(glob.glob(r'{0}\\[[]*.*[]]*{2}'.format(filmsPath, fileRegex, extention)))
+        filesList+=(glob.glob(r'{0}\\[[]*.*[]]*{1}'.format(filmsPath, extention)))
 
 #correct files' titles
-[print(f) for f in filesList]
+titleStart = r'\[.*[.].*\] '
+#[print(re.sub(titleStart,'',f)) for f in filesList]
+[os.rename(f, re.sub(titleStart,'',f)) for f in filesList]
